ae442f2f602705d37f3ffdf27c860caaf9c702fd
Khushboo <khushboopal176@gmail.com>
2020-06-14 01:54:27 +0530

    merge

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
-
 const app = express()
 const PORT = 3000
 
@@ -15,41 +14,19 @@ hbs.registerPartials(PartialsPath)
 
 app.use(express.static(PublicDirPath))
 
-// app.use(express.static('public'))
+app.use(express.static('public'))
 
-app.set('view engine', 'hbs')
 
-app.get('/', (req,res) =>{
 
-      res.render('home' , {
-          title : 'Donate India'
+
+app.get('/donate', (req,res) =>{
+
+      res.render('donate-form'  ,{
+
       })
 
     
 })
-
-app.get('/about', (req,res) =>{
-    
-    res.render('about' , {
-
-    })
-})
-
-app.get('/campaign' , (req,res) => {
-
-    res.render('campaign' , {
-        
-    })
-})
-
-app.get('/donate' , (req,res) => {
-    
-    res.render('donate-form' , {
-
-    })
-})
-
-
 
 
 
